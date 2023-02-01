c4194d9218b03a6023b3845a7b7de75d9e60cb16
Alessandro Lobo <alessandro.lobo@hotmail.com>
2023-02-01 09:27:28 -0300

    finalizado map endpoints

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,6 +4,8 @@ import PokemonCard from '../../components/PokemonCard'
 import { Container, Grid } from '@mui/material'
 import axios from 'axios'
 
+//video 39:10
+
 export const Home = () => {
   const [pokemons, setPokemons] = useState([])
 
@@ -13,32 +15,42 @@ export const Home = () => {
 
 
   const getPokemons = () => {
-    axios.get("https://pokeapi.co/api/v2/pokemon")
+    var endpoints = []
+    for (var i = 1; i < 50; i++) {
+      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+
+    }
+    console.log(endpoints)
+    axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => setPokemons(res)).catch((err) => console.error(err));
+
+    const pokemonFilter = (name) => {
+      var filteredPokemons = []
+      for (var i in pokemons) {
+        if (pokemons[i].name.include(name)) {
+          filteredPokemons.push(pokemons[i]);
+        }
+      }
+      setPokemons(filteredPokemons);
+    }
+    /*  axios
+      .get("https://pokeapi.co/api/v2/pokemon/")
       .then((res) => {
         console.log(res.data.results)
         setPokemons(res.data.results)
       })
-      .catch((err) => console.error(err));
+      .catch((err) => console.error(err)); */
   }
 
   return (
     <div>
-      <Navbar />
+      <Navbar pokemonFilter={pokemonFilter} />
 
       <Container maxWidth="false">
-        <Grid container>
-          <Grid item xs={3}>
-            <PokemonCard />
-          </Grid>
-          <Grid item xs={3}>
-            <PokemonCard />
-          </Grid>
-          <Grid item xs={3}>
-            <PokemonCard />
-          </Grid>
-          <Grid item xs={3}>
-            <PokemonCard />
-          </Grid>
+        <Grid container spacing={3}>
+          {pokemons.map((pokemon, key) => (<Grid item xs={2} key={key}>
+            <PokemonCard name={pokemon.data.name} image={pokemon.data.sprites.front_default} />
+          </Grid>))}
+
         </Grid>
       </Container>
     </div>
